c8b2f8cad9d920c6e0a2edb47e5bcf3620cbef39
WOULDU-pres <jhw664089@gmail.com>
2023-01-27 09:20:31 +0900

    Update urls.py

--- django/projects/mysite/config/urls.py
+++ django/projects/mysite/config/urls.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from pybo import views  # 더 이상 필요하지 않으므로 삭제
 
+app_name = 'pybo'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pybo/', include('pybo.urls')), 
+    path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'),
 ]
